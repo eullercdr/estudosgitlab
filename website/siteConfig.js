318a872be41d3118a8ba13433f006d42a0bf5f1b
Euller Cristian <eullercdr@gmail.com>
2019-03-17 09:07:59 -0300

    Alterando o build do projeto

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'Devops Site', // Title for your website.
-  tagline: 'A website for testing',
+  tagline: 'Euller Cristian, estudos devop',
   url: 'https://your-docusaurus-test-site.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
@@ -30,7 +30,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'missaodevops',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
